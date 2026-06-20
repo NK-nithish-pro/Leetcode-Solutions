b7474c8d53da9b48da267c6de7238f7dfa1bf22e
NK-nithish-pro <snithishkumar999@gmail.com>
2026-06-20 18:58:47 +0530

    1732_Find_the_highest_altitude.py

--- 1732_Find_the_highest_altitude.py
+++ 1732_Find_the_highest_altitude.py
@@ -1,4 +1,8 @@
+# LeetCode 1732 - Find the Highest Altitude
+# Difficulty: Easy
+# Solved: 19-06-2026
 
+class Solution:
 def largestAltitude(self, gain: List[int]) -> int:
         altitudes = [0]
         point=0
