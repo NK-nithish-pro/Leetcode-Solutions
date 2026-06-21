bf3b3240f90a44b8599a7fef3e2ee49daae53529
NK-nithish-pro <snithishkumar999@gmail.com>
2026-06-21 14:20:46 +0530

    Update and rename 1833_Maximum_Icecream_bars.py to Python/Medium/1833_Maximum_Icecream_bars.py

--- Python/Medium/1833_Maximum_Icecream_bars.py
+++ Python/Medium/1833_Maximum_Icecream_bars.py
@@ -3,6 +3,9 @@
 #Difficulty - Medium
 #Solved on 21.06.26
 
+#Same idea but different implementations
+
+#1) Tracks the total money and checks whether it is exceeding the coins
 class Solution:
     def maxIceCream(self, costs: List[int], coins: int) -> int:
 
@@ -17,7 +20,8 @@ class Solution:
                 break    
         return maxIce
 
-#Alternate solution
+#Alternate solution - tracks how much total money is spent and checks whether coins is available enough each time
+
 class Solution:
     def maxIceCream(self, costs: List[int], coins: int) -> int:
         maxIce=0
