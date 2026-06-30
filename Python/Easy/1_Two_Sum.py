d69da572cf70338755e34d4bb7f53c5479425868
Nithish Kumar S <snithishkumar999@gmail.com>
2026-06-30 21:08:17 +0530

    Added problem 1

---
+++ Python/Easy/1_Two_Sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        indices=[]
+        for i in range(len(nums)-1):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j]==target:
+                    indices.extend([i,j])
+        return indices
